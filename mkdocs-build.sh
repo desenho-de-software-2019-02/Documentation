4232b8c2afe1aa2a41b1417704f985327a18b977
lmmLucasMachado <lmlucasmachadom@gmail.com>
2019-09-10 00:23:05 -0300

    Preparando ambiente

--- mkdocs-build.sh
+++ mkdocs-build.sh
@@ -6,9 +6,8 @@ rm -rf public
 # config
 git config --global user.email "${GH_USER_EMAIL}"
 git config --global user.name "${GH_USER_NAME}"
-git remote add gh-token "https://${GH_TOKEN}@github.com/desenho-de-software-2019-02/Document-o";
+git remote add gh-token "https://${GH_TOKEN}@github.com/fga-disciplinas/2019.1-Guia-Bolso.git";
 git fetch gh-token && git fetch gh-token gh-pages:gh-pages;
 mkdocs gh-deploy --clean --remote-name gh-token;
 
 echo "Completed deploying documentation"
-
